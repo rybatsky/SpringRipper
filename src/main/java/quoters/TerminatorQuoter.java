f6fb609b13547ce29dc49b02f2c1ea671732ff53
rybatsky <stacyrybakova@gmail.com>
2018-02-07 13:16:36 +0300

    PostProxy added

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,13 +1,27 @@
 package quoters;
 
+import javax.annotation.PostConstruct;
+
 @Profiling
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
     private String message;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
+
     @Override
+    @PostProxy
     public void sayQuote() {
+        System.out.println("Phase 3");
         for (int i = 0; i < repeat; i++) {
             System.out.print(message + "\n");
         }
