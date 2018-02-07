fa31a7bc8e8dfcc506cd14a472613e78ec8d7ac3
rybatsky <stacyrybakova@gmail.com>
2018-02-07 15:23:20 +0300

    context.properties added

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -3,6 +3,7 @@ package quoters;
 import javax.annotation.PostConstruct;
 
 @Profiling
+@DeprecatedClass(newImpl = T1000.class)
 public class TerminatorQuoter implements Quoter {
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
@@ -33,5 +34,9 @@ public class TerminatorQuoter implements Quoter {
 
     public int getRepeat() {
         return repeat;
+    }
+
+    public void setRepeat(int repeat) {
+        this.repeat = repeat;
     }
 }
